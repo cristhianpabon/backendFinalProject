057eafd077ed668bcb1427c73dd0d23684fb4e25
Cristhian Pabon <Cristhianpabon89@gmail.com>
2024-12-18 00:22:19 -0300

    Agregando las rutas

--- index.js
+++ index.js
@@ -1,47 +1,15 @@
-const express = require("express");
-const port = 8080;
+const express = require('express');
+const bodyParser = require('body-parser');
+const productsRouter = require('./routes/product.router');
+const cartRouter = require('./routes/cart.router'); 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("¡Hola desde Express!");
-});
+app.use(bodyParser.json());
 
-//products
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartRouter); 
 
-app.get("/api/products/", (req, res) => {
-  res.send("¡Hola desde Express!");
-});
-
-app.get("/api/products/:id", (req, res) => {
-  res.send("¡Hola desde Express!");
-});
-
-app.post("/api/products/", (req, res) => {
-  res.send("¡Hola desde Express!");
-});
-
-app.put("/api/products/:pid", (req, res) => {
-  res.send("¡Hola desde Express!");
-});
-
-app.delete("/api/products/:pid", (req, res) => {
-  res.send("¡Hola desde Express!");
-});
-
-//carts
-
-app.post("/api/carts/", (req, res) => {
-  res.send("¡Hola desde Express!");
-});
-
-app.get("/api/carts/:cid", (req, res) => {
-  res.send("¡Hola desde Express!");
-});
-
-app.post("/api/carts/:cid/product/:pid", (req, res) => {
-  res.send("¡Hola desde Express!");
-});
-
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
